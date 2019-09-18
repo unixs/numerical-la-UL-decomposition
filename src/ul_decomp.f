7b0c6a00fb8e1809bffc1d82067e2624f58af5d8
Alexander Feodorov <webmaster@unixcomp.org>
2019-09-18 15:23:58 +0300

    Add format

--- src/ul_decomp.f
+++ src/ul_decomp.f
@@ -2,9 +2,10 @@ C     UL Matrix decomposition
       PROGRAM UL_DECOMP
         REAL FACT, P
 C        DIMENSION FACT(3,3)
-        DATA FACT, P /10.25, 0.1/
+        DATA FACT, P /10.25, .1/
 
         FACT = FACT + P
 
-        PRINT *, "UL-DECOMP", FACT, P
+        PRINT 100, "UL-DECOMP", FACT, P
+100     FORMAT (1X, A, 1X, 2F15.12)
       END
