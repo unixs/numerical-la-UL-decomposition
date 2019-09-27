1bdcc6c5af39751eaf09918719bcadee1678aa36
Alexander Feodorov <aleksandr_fedorov2@epam.com>
2019-09-27 18:05:39 +0300

    Add some code

--- src/ul_decomp.f
+++ src/ul_decomp.f
@@ -1,11 +1,32 @@
 C     UL Matrix decomposition
       PROGRAM UL_DECOMP
-        REAL FACT, P
-C        DIMENSION FACT(3,3)
-        DATA FACT, P /10.25, .1/
+        PARAMETER (N = 3)
+        REAL M, V, U, L, Y, X
+        DIMENSION M(N,N), V(N), U(N,N), L(N,N), Y(N), X(N)
+        DATA M /1., -1., 3., 4., -2., 20., 2., 1., 19/,
+     &       V /8., 3., 71./,
+     &       U /9 * 0/,
+     &       L /9 * 0/
 
-        FACT = FACT + P
 
-        PRINT 100, "UL-DECOMP", FACT, P
-100     FORMAT (1X, A, 1X, 2F15.12)
+        DO 10 I = 1, N
+C         Fill L matrix diag with 1
+          L(I, I) = 1
+C         Fill U matrix first row
+          U(1, I) = M(1, I)
+
+C         Fill L matrix first column
+          IF (I.GT.1)
+     &      L(I, 1) = M(I, 1) / U(1, 1)
+10      CONTINUE
+
+        DO 30 I = 2, N
+          DO 20 J = I, N
+            U(I, J) = M(I, J) - SUM()
+            L(J, I) = (1 / U(J, J)) * (M(J, I) - SUM())
+20        CONTINUE
+30      CONTINUE
+
+        PRINT 500, "UL-DECOMP", M(3, 2), V(2), U(1, 2), L(3, 1)
+500     FORMAT (1X, A, 1X, 4F15.12)
       END
