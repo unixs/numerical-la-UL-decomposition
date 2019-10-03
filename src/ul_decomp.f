86f6ef609745c6641a65839f4c1e6ed9eb1e82b4
Alexander Feodorov <aleksandr_fedorov2@epam.com>
2019-10-03 19:02:31 +0300

    Continue

--- src/ul_decomp.f
+++ src/ul_decomp.f
@@ -1,10 +1,10 @@
 C     UL Matrix decomposition
       PROGRAM UL_DECOMP
         PARAMETER (N = 3)
-        REAL M, V, U, L, Y, X
-        DIMENSION M(N,N), V(N), U(N,N), L(N,N), Y(N), X(N)
-        DATA M /1., -1., 3., 4., -2., 20., 2., 1., 19/,
-     &       V /8., 3., 71./,
+        REAL A, B, U, L, Y, X
+        DIMENSION A(N,N), B(N), U(N,N), L(N,N), Y(N), X(N)
+        DATA A /1., -1., 3., 4., -2., 20., 2., 1., 19/,
+     &       B /8., 3., 71./,
      &       U /9 * 0/,
      &       L /9 * 0/
 
@@ -13,20 +13,33 @@ C     UL Matrix decomposition
 C         Fill L matrix diag with 1
           L(I, I) = 1
 C         Fill U matrix first row
-          U(1, I) = M(1, I)
+          U(1, I) = A(1, I)
 
 C         Fill L matrix first column
           IF (I.GT.1)
-     &      L(I, 1) = M(I, 1) / U(1, 1)
+     &      L(I, 1) = A(I, 1) / U(1, 1)
 10      CONTINUE
 
         DO 30 I = 2, N
           DO 20 J = I, N
-            U(I, J) = M(I, J) - SUM()
-            L(J, I) = (1 / U(J, J)) * (M(J, I) - SUM())
+
+C           Fill U row cell by cell
+            S = 0
+            DO 15 K = 1, J - 1
+              S = S + L(J, K) * U(K, I)
+15          CONTINUE
+            U(I, J) = A(I, J) - S
+
+C           Fill L column cell by cell & use previously calculated cell
+            S = 0
+            DO 16 K = 1, J - 1
+              S = S + L(I, K) * U(K, J)
+16          CONTINUE
+            L(J, I) = (1 / U(J, J)) * (A(J, I) - S)
+
 20        CONTINUE
 30      CONTINUE
 
-        PRINT 500, "UL-DECOMP", M(3, 2), V(2), U(1, 2), L(3, 1)
+        PRINT 500, "UL-DECOMP", A(3, 2), B(2), U(1, 2), L(3, 1)
 500     FORMAT (1X, A, 1X, 4F15.12)
       END
