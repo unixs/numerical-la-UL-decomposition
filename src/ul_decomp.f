294163c7f4c59654037190f26fd219ffc86edac3
Alexander Feodorov <webmaster@unixcomp.org>
2019-10-03 22:07:24 +0300

    Complete calculation

--- src/ul_decomp.f
+++ src/ul_decomp.f
@@ -25,22 +25,42 @@ C         Fill L matrix first column
 
 C           Fill U row cell by cell
             S = 0
-            DO 15 K = 1, J - 1
-              S = S + L(J, K) * U(K, I)
+            DO 15 K = 1, I - 1
+              S = S + L(I, K) * U(K, J)
 15          CONTINUE
             U(I, J) = A(I, J) - S
 
 C           Fill L column cell by cell & use previously calculated cell
             IF (J.GT.I) THEN
               S = 0
-              DO 16 K = 1, J - 1
-                S = S + L(I, K) * U(K, J)
-  16          CONTINUE
+              DO 16 K = 1, I - 1
+                S = S + L(J, K) * U(K, I)
+16            CONTINUE
               L(J, I) = (1 / U(I, I)) * (A(J, I) - S)
             END IF
 
 20        CONTINUE
 30      CONTINUE
+
+C       y: Ly = b
+        DO 50 I = 1, N
+          S = 0
+          DO 40 K = 1, I - 1
+            S = S + L(I, K) * Y(K)
+40        CONTINUE
+
+          Y(I) = B(I) - S
+50      CONTINUE
+
+C       x: Ux = y
+        DO 70 I = N, 1, -1
+          S = 0
+          DO 60 K = 1, I - 1
+            S = S + U(I, K) * X(K)
+60        CONTINUE
+
+          X(I) = Y(I) - S
+70      CONTINUE
 
         PRINT 500, "UL-DECOMP", A(3, 2), B(2), U(1, 2), L(3, 1)
 500     FORMAT (1X, A, 1X, 4F15.12)
